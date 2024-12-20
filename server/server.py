f6cc3fc5c32551d8ec5f101d16dd4e5178e91477
QuanOnMintLatitude5400 <98805733+quan11304@users.noreply.github.com>
2024-12-20 16:44:13 +0700

    Updated server
    Remove commented code from Python file. Change port to 80

--- server/server.py
+++ server/server.py
@@ -10,43 +10,6 @@ def randomiser():
     else:
         return f'<b>Sorry, you got {r}</b>'
 
-#
-# # Exercise 2
-# @app.route('/welcome')
-# def welcome():
-#     return '''Welcome to Flask Development!
-# <br>
-# This is Labwork 3: Flask/MySQL/API
-# '''
-#
-# # Exercise 3
-# @app.route('/table')
-# def table():
-#     data = [{'name': 'Alice', 'age': 22},
-#             {'name': 'Bob', 'age': 19},
-#             {'name': 'Charlie', 'age': 25},
-#             {'name': 'David', 'age': 24},
-#             {'name': 'Eve', 'age': 21}]
-#     return render_template('table.html', students=data)
-#
-# # Exercise 4
-# @app.route('/factorial/<number>')
-# def calc(number):
-#     return str(factorial(int(number)))+"\n"
-#
-# # Exercise 5
-# @app.route('/is_prime/<number>')
-# def prime(number):
-#     if isprime(int(number)):
-#         return "Prime"
-#     else:
-#         return "Not prime"
-#
-# # Exercise 6
-# @app.route("/sort")
-# def sort_flask():
-#     numbers = request.args.get("numbers").split(",")
-#     return str(sorted(numbers))
 
 if __name__ == '__main__':
     app.run(debug=True)
